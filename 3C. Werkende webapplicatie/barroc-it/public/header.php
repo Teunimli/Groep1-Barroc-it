22b4c6e7f4b3195c28a72896ca6a3b74e2e33c25
Evelien Rookmaker <Studie@rookmaker.nl>
2015-10-06 11:13:23 +0200

    Projectstructuur
    Projectstructuur
    Projectstructuur toegevoegd.
    Header, index en footer gescheiden.

--- 3C. Werkende webapplicatie/barroc-it/public/header.php
+++ 3C. Werkende webapplicatie/barroc-it/public/header.php
@@ -1,3 +1,27 @@
 <?php require __DIR__ . '/../app/init.php'; ?>
 
+<!DOCTYPE html>
+<!--[if lt IE 7]>     <!-- <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>       <!--  <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <!--<html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+
+<!-- Mirrored from barroc.radiusdev.nl/ by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 01 Oct 2015 07:21:52 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="assets/css/boostrap-theme.min.html"/>
+    <link rel="stylesheet" href="assets/css/main.css"/>
+
+    <script src="assets/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+
+</head>
+<body>
+
 
--- 3C. Werkende webapplicatie/barroc-it/public/login.php
+++ 3C. Werkende webapplicatie/barroc-it/public/login.php
@@ -1,28 +1,6 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    
-<!-- Mirrored from barroc.radiusdev.nl/login.php by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 01 Oct 2015 07:22:13 GMT -->
-<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
-<head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php require_once __DIR__ . '/header.php';?>
 
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-
-        <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="assets/css/main.css">
-
-        <script src="assets/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-        
-    </head>
-    <body>
- <div class="jumbotron jumbo-login">
+<div class="jumbotron jumbo-login">
       <div class="container">
         <h1 class="text_1 text-center">BARROC IT. </h1>
         <form action="#" class="form-login col-md-4 col-md-offset-4" >
@@ -41,3 +19,6 @@
         </form>
       </div>
  </div>
+
+
+<?php require_once __DIR__ . '/footer.php';?>
