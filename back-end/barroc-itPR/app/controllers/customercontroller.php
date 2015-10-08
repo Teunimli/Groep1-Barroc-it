2a1e415eb12e48be3eef63a5a88ee8461ba89462
Jorrit076 <jorritjeah@gmail.com>
2015-10-08 10:45:31 +0200

    back-end test

---
+++ back-end/barroc-itPR/app/controllers/customercontroller.php
@@ -0,0 +1,123 @@
+<?php
+require_once '../init.php';
+switch( $_POST['type'] ) {
+    case 'add' :
+        add($_POST['contact_name'],
+            $_POST['contact_lastname'],
+            $_POST['companyname'],
+            $_POST['first_adress'],
+            $_POST['first_zipcode'],
+            $_POST['first_city'],
+            $_POST['first_housenumber'],
+            $_POST['second_adress'],
+            $_POST['second_zipcode'],
+            $_POST['second_city'],
+            $_POST['second_housenumber'],
+            $_POST['contactperson'],
+            $_POST['initials'],
+            $_POST['first_telephonenumber'],
+            $_POST['second_telephonenumber'],
+            $_POST['fax'],
+            $_POST['email'],
+            $_POST['ledgeraccountnumber'],
+            $_POST['taxcode'],
+            $db );
+        break;
+    case 'edit' :
+        break;
+    case 'delete' :
+        $id = filter_var($_POST['id'], FILTER_SANITIZE_NUMBER_INT);
+        remove($db, $_POST['id']);
+        break;
+}
+
+function alert($string)
+{
+    echo '<script type="text/javascript">alert("' . $string . '");</script>';
+}
+
+function add($contact_name, $contact_lastname,
+             $companyname,$first_adress,
+             $first_zipcode,$first_city, $first_housenumber,
+             $second_adress, $second_zipcode, $second_city,
+             $second_housenumber, $contactperson, $initials,
+             $first_telephonenumber, $second_telephonenumber,
+             $fax, $email, $ledgeraccountnumber, $taxcode, $db) {
+
+    $controle = 2;
+
+    $created_at = time();
+
+    $sql = "SELECT * FROM tbl_customer WHERE companyname = :companyname";
+    $q = $db->prepare($sql);
+    $q->bindParam(':companyname', $companyname);
+    $q->execute();
+
+    // kijkt of de rij al bestaat
+    if ($q->rowCount() > 0) {
+        $exists = 'customer already exists';
+        alert($exists);
+        $controle = 1;
+
+    } else if(
+        empty($_POST['contact_name']) &&
+        empty($_POST['contact_lastname']) &&
+        empty($_POST['companyname']) &&
+        empty($_POST['first_adress']) &&
+        empty($_POST['first_zipcode']) &&
+        empty($_POST['first_city']) &&
+        empty($_POST['first_housenumber']) &&
+        empty($_POST['contactperson']) &&
+        empty($_POST['initials']) &&
+        empty($_POST['first_telephonenumber']) &&
+        empty($_POST['email']) ) {
+
+
+        $notFilled = 'data is incomplete';
+        alert($notFilled);
+        $controle = 1;
+        header('location:' . HTTP . '/public/views/customers/add.php');
+    }
+
+    if($controle == 2) {
+
+
+        $sql = "INSERT INTO tbl_customer (contact_name, contact_lastname, companyname,
+                                      first_adress, first_zipcode, first_city,
+                                      first_housenumber, second_adress, second_zipcode,
+                                      second_city, second_housenumber, contactperson, initials,
+                                      first_telephonenumber, second_telephonenumber, fax, email,
+                                      ledgeraccountnumber, taxcode, created_at )
+                    VALUES (:contact_name, :contact_lastname, :companyname, :first_adress
+                            :first_zipcode, :first_city, :first_housenumber, :second_adress,
+                            :second_zipcode, :second_city, :second_housenumber, :contactperson,
+                            :initials, :first_telephonenumber, :second_telephonenumber, :fax, :email,
+                            :ledgeraccountnumber, :taxcode, :created_at )";
+
+
+        $q = $db->prepare($sql);
+        $q->bindParam(':contact_name', $contact_name);
+        $q->bindParam(':contact_lastname', $contact_lastname);
+        $q->bindParam(':companyname', $companyname);
+        $q->bindParam(':first_adress', $first_adress);
+        $q->bindParam(':first_zipcode', $first_zipcode);
+        $q->bindParam(':first_city', $first_city);
+        $q->bindParam(':first_housenumber', $first_housenumber);
+        $q->bindParam(':second_adress', $second_adress);
+        $q->bindParam(':second_zipcode', $second_zipcode);
+        $q->bindParam(':second_city', $second_city);
+        $q->bindParam(':second_housenumber', $second_housenumber);
+        $q->bindParam(':contactperson', $contactperson);
+        $q->bindParam(':initials', $initials);
+        $q->bindParam(':first_telephonenumber', $first_telephonenumber);
+        $q->bindParam(':second_telephonenumber', $second_telephonenumber);
+        $q->bindParam(':fax', $fax);
+        $q->bindParam(':email', $email);
+        $q->bindParam(':ledgeraccountnumber', $ledgeraccountnumber);
+        $q->bindParam(':taxcode', $taxcode);
+        $q->bindParam(':created_at', $created_at);
+        $q->execute();
+
+        header('location:' . HTTP . '/public/index.php');
+    }
+}
